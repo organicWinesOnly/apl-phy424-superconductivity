6cc432b9b18e20e3e2e420dbf83ab7f85ccc3939
rhamel.mw <rhamel.rdelpeache@gmail.com>
2022-09-05 13:31:47 -0400

    Add docstrings for LockIn7270

--- src/event_handler.py
+++ src/event_handler.py
@@ -15,17 +15,12 @@ class EventHandler:
 
     def run(self, set_voltage, sample_rate=10000, len_=100000):
         print("Begining data aquisition...")
-        scope_output = []
-        lock_in_ouput = []
         self.lock_in.set_up()
         self.lock_in.curve_setup(sample_rate, len_)
 
         events = [th.Event() for i in range(3)]
         lock = th.Lock()  # protect global variables
         curr_fin_voltage = (1000, set_voltage)
-        # global _curr_fin_voltage  # (current, set_voltage)
-        #final_voltage = set_voltage
-        #current_voltage = 1000 #mV
 
         # initialie threads
         scope_thread = th.Thread(
@@ -49,28 +44,6 @@ class EventHandler:
         lock_in_thread.join()
         print("Run complete.")
 
-
     def save_data(self):
         self.data.update_header(self.lock_in.data)
         self.data.update_header(self.lock_in.data)
-
-        
-
-if __name__ == "__main__":
-    b = th.Barrier(3, timeout=1)
-    event = EventHandler()
-    scope_output = []
-    lock_in_ouput = []
-    print("Collecting data...")
-
-    # Set up scope
-    set_dvm(scope)
-    set_trigger(scope)
-    scope_thread = th.Thread(target=run_scope, args=(scope, scope_output, b))
-    scope_thread.start()
-
-    # set up lock-in
-    lock_in_thread = th.Thread(target=run_lock_in, args=(lock_in, lock_in_output,
-        b))
-    lock_in_thread.start()
-    b.wait()
--- src/lockin_7270.py
+++ src/lockin_7270.py
@@ -13,7 +13,24 @@ SETUP_CMDS = {
 
 
 class LockIn7270:
-    """
+    """ Instance of an Ametek 7270 lock in amplifier
+
+
+       Public Methods
+       --------------
+       query(..) - write and read commands to device
+       setup(..) - update device settings
+       curve_setup(..) - update device settings for new curve measurement
+       run(..) - complete curve measurements
+       read_curve(..) - read curve data for a single attribute into data buffer
+       read_all_curves - read all curves into data buffer
+
+       Public Attributes
+       ----------------
+       dev - lock in device
+       data - dictionary of stored data measurements
+       ep_in/_out - location of in and out usb endpoints respectively
+       
         # dev.write(1, "REFMODE 0")  # single reference mode
         # dev.write(1, "IE 0")  # internal reference
         # dev.write(1, "VMODE 3")  # A-B differential mode
@@ -25,8 +42,17 @@ class LockIn7270:
         # dev.write(1, "FET 0")  # bipolar device
     """
     def __init__(self, id_vendor=2605, id_product=27, cmds=SETUP_CMDS):
-        # ID_VENDOR = 2605  # product specific details of the lock_in amplifier used
-        # ID_PRODUCT = 27
+        """ Initialize class.
+         
+            Paramaters
+            ----------
+            id_vendor : int 
+                ...
+            id_product : int 
+                ...
+            cmds : dict
+                ...
+        """
         self.dev = usb.core.find(idVendor=id_vendor, idProduct=id_product)
         self.dev.set_configuration()
         self.paramaters = cmds
@@ -35,21 +61,43 @@ class LockIn7270:
         self.ep_out = self.dev[0].interfaces()[0].endpoints()[0]  # bulk out
         self.data = {0: [], 1: [], 3: [], 4: [], 5: []}  
 
-    def query(self, cmd):
+    def query(self, cmd, silent=False):
+        """ Write and read a command to the lock in instrument.
+            
+            Paramaters
+            ----------
+            cmd : str
+                The command.
+            silent : bool
+                Wheter the read message should be printed to the screen.
+        """
         self.dev.write(1, cmd)
         try:
             output = self.dev.read(
                 self.ep_in.bEndpointAddress, 
                 self.ep_in.wMaxPacketSize)
-            print(output.tobytes().decode("utf-8"))
+            if !silent:
+                print(output.tobytes().decode("utf-8"))
         except:
-            print('')
+            if !silent:
+                print('')
 
     def set_up(self):
+        """ Write settings based on the parameter attribute. """
         for cmd, value in self.paramaters.items():
             self.dev.write(1, "{} {}".format(cmd, value))
 
     def curve_setup(self, sample_rate=10000, len_=100000):
+        """ Initialize curve collection settings of the device.
+
+            Paramaters
+            ----------
+            sample_rate : int
+                Time steps between measurements in microseconds (minimum: 1 - in fast
+                mode; 1000 standard mode)
+            len_ : int
+                Number of measuremnts to store (max = 100,000)
+        """
         self.query("NC")  # clear curve buffer
         self.query("CMODE 0")  # set curve aquistion to fast mode
         self.query("CBD 59")  # store X,Y, phase, sensitivity, Noise
@@ -60,6 +108,16 @@ class LockIn7270:
         self.query("REFP 0")  # set phase to 0
 
     def read_curve(self, bit_):
+        """ Read and save curve data for the specified <bit_> value. 
+            
+            Read curve data, convert data to floating point numbers, and store
+            results in data attribute at corresponding location.
+            
+            Paramaters
+            ----------
+            bit_ : int
+                ....
+        """
         output = array.array('B')
         self.dev.write(1, "DCB {}".format(bit_))
         while True:
@@ -80,6 +138,8 @@ class LockIn7270:
         self.data[bit_].extend(data)
 
     def read_all_curves(self):
+        """ Save all curves stored in device buffer to computer.
+        """
         for i in self.data.keys():
             self.read_curve(i)
 
@@ -87,6 +147,8 @@ class LockIn7270:
     def run(self, curr_fin_voltage,
             events, lock, 
             sample_rate=10000, len_=1000000):
+        """ Run curve aquistion.
+        """
         print("Lock in running.")
         with lock:
             curr = curr_fin_voltage[0]
@@ -103,15 +165,4 @@ class LockIn7270:
             self.read_all_curves()
             events[1].wait()
             print("Lock in: data saved")
-
-
-    def query(self, cmd):
-        self.dev.write(1, cmd)
-        try:
-            output = self.dev.read(
-                self.ep_in.bEndpointAddress, 
-                self.ep_in.wMaxPacketSize)
-            print(output.tobytes().decode("utf-8"))
-        except:
-            print('')
 
